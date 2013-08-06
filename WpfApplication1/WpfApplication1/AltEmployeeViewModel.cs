3ee61944288d7fe9d1f0c79a56f2b5534e393a53
Alan Chapman <alanclv@gmail.com>
2013-08-05 22:05:06 -0700

    Initial Commit

---
+++ WpfApplication1/WpfApplication1/AltEmployeeViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class AltEmployeeViewModel
+    {
+        //#region Members
+        //private string _afirstName;
+        //private string _lastName;
+        //private string _employeeName;
+        //#endregion
+
+        //public string Test { get; set; }
+        //public string Aprop { get; set; }
+        //#region Properties
+        //public string AFirstName
+        //{
+        //    get { return _afirstName; }
+        //    set { _afirstName = value; }
+        //}
+
+        //public string LastName
+        //{
+        //    get { return _lastName; }
+        //    set { _lastName = value; }
+        //}
+
+        //public string EmployeeName
+        //{
+        //    get
+        //    {
+        //        return _employeeName;
+        //    }
+        //    set
+        //    {
+        //        _employeeName = value;
+        //    }
+        //}
+        //#endregion
+    }
+}
---
+++ WpfApplication1/WpfApplication1/EmployeeViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+//using Microsoft.Practices.Composite.Presentation.Commands;
+
+
+namespace WpfApplication1
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private string _employeeName = null;
+        private string _firstName = null;
+        private string _lastName = null;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DelegateCommand<EmployeeViewModel> SubmitCommand { get; private set; }
+        public bool SubmitEnabled { get; private set; }
+        
+        private void OnSubmit(object arg) 
+        { 
+            EmployeeName = "Entered employee: " + FirstName + " " + LastName; 
+        }
+        
+        private bool CanSubmit(object arg) 
+        { 
+            bool test = !(String.IsNullOrEmpty(FirstName) || String.IsNullOrEmpty(LastName));
+            return test; 
+        }
+
+        public EmployeeViewModel()
+        {
+            this.SubmitCommand = new DelegateCommand<EmployeeViewModel>(this.OnSubmit, this.CanSubmit);
+            SubmitCommand.CanExecuteChanged += tester;
+        }
+
+        public string EmployeeName
+        {
+            get
+            {
+                return this._employeeName;
+            }
+
+            set
+            {
+                this._employeeName = value;
+                RaisePropertyChanged("EmployeeName");
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this._firstName;
+            }
+
+            set
+            {
+                this._firstName = value;
+                RaisePropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this._lastName;
+            }
+
+            set
+            {
+                this._lastName = value;
+                RaisePropertyChanged("LastName");
+            }
+        }
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+            this.SubmitEnabled = this.SubmitCommand.CanExecute(null);
+            SubmitCommand.RaiseCanExecuteChanged();
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public void tester(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test worked");
+        }
+    }
+}
